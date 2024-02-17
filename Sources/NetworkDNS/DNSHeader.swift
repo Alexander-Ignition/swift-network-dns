053d647f2abc773cef1e08735f5bebe93af4cb11
Alexander Ignition <izh.sever@gmail.com>
2024-02-17 21:11:29 +0300

    Update DNSHeader.swift
    - Added Parameters as OptionSet
    - Removed Mask

--- Sources/NetworkDNS/DNSHeader.swift
+++ Sources/NetworkDNS/DNSHeader.swift
@@ -13,15 +13,17 @@ import Foundation
 public struct DNSHeader: Hashable, Sendable, Identifiable {
     public static let length = 12 // number of bytes [UInt8]
 
-    enum Mask {
-        static let qr:      UInt16 = 0b1000_0000_0000_0000
-        static let opcode:  UInt16 = 0b0111_1000_0000_0000
-        static let aa:      UInt16 = 0b0000_0100_0000_0000
-        static let tc:      UInt16 = 0b0000_0010_0000_0000
-        static let rd:      UInt16 = 0b0000_0001_0000_0000
-        static let ra:      UInt16 = 0b0000_0000_1000_0000
-        static let z:       UInt16 = 0b0000_0000_0111_0000
-        static let rcode:   UInt16 = 0b0000_0000_0000_1111
+    struct Parameters: OptionSet, Hashable, Sendable {
+        var rawValue: UInt16
+
+        static let qr       = Parameters(rawValue: 0b1000_0000_0000_0000)
+        static let opcode   = Parameters(rawValue: 0b0111_1000_0000_0000)
+        static let aa       = Parameters(rawValue: 0b0000_0100_0000_0000)
+        static let tc       = Parameters(rawValue: 0b0000_0010_0000_0000)
+        static let rd       = Parameters(rawValue: 0b0000_0001_0000_0000)
+        static let ra       = Parameters(rawValue: 0b0000_0000_1000_0000)
+        static let z        = Parameters(rawValue: 0b0000_0000_0111_0000)
+        static let rcode    = Parameters(rawValue: 0b0000_0000_0000_1111)
     }
 
     /// A 16 bit identifier assigned by the program that generates any kind of query.
@@ -31,18 +33,18 @@ public struct DNSHeader: Hashable, Sendable, Identifiable {
     public var id: UInt16
 
     /// QR, Opcode, AA, TC, RD, RA, Z , RCODE
-    var parameters: UInt16 = 0
+    var parameters = Parameters(rawValue: 0)
 
     /// QR. A one bit field that specifies whether this message is a query (0), or a response (1).
     public var isResponse: Bool {
-        ((parameters & Mask.qr) >> 15) == 1
+        parameters.contains(.qr)
     }
 
     /// A four bit field that specifies kind of query in this message.
     ///
     /// This value is set by the originator of a query and copied into the response.
     public var opcode: DNSOpcode {
-        DNSOpcode(rawValue: (parameters & Mask.opcode) >> 11)
+        DNSOpcode(rawValue: (parameters.rawValue & Parameters.opcode.rawValue) >> 11)
     }
 
     /// AA. Authoritative Answer - this bit is valid in responses,
@@ -52,12 +54,12 @@ public struct DNSHeader: Hashable, Sendable, Identifiable {
     /// Note that the contents of the answer section may have multiple owner names because of aliases.
     /// The AA bit corresponds to the name which matches the query name, or the first owner name in the answer section.
     public var isAuthoritativeAnswer: Bool {
-        ((parameters & Mask.aa) >> 10) == 1
+        parameters.contains(.aa)
     }
 
     /// TrunCation - specifies that this message was truncated due to length greater than that permitted on the transmission channel.
     public var isTrunCation: Bool {
-        ((parameters & Mask.tc) >> 9) == 1
+        parameters.contains(.tc)
     }
 
     /// Recursion Desired - this bit may be set in a query and is copied into the response.
@@ -65,27 +67,31 @@ public struct DNSHeader: Hashable, Sendable, Identifiable {
     /// If RD is set, it directs the name server to pursue the query recursively. Recursive query support is optional.
     public var isRecursionDesired: Bool {
         get {
-            ((parameters & Mask.rd) >> 8) == 1
+            parameters.contains(.rd)
         }
         set {
-            parameters = (parameters & ~(1 << 8))
+            if newValue {
+                parameters.insert(.rd)
+            } else {
+                parameters.remove(.rd)
+            }
         }
     }
 
     /// Recursion Available - this be is set or cleared in a response, and denotes
     /// whether recursive query support is available in the name server.
     public var isRecursionAvailable: Bool {
-        ((parameters & Mask.ra) >> 7) == 1
+        parameters.contains(.ra)
     }
 
     /// Z Reserved for future use.  Must be zero in all queries and responses.
     var z: UInt16 {
-        ((parameters & Mask.z) >> 4)
+        (parameters.rawValue & Parameters.z.rawValue) >> 4
     }
 
     /// Response code - this 4 bit field is set as part of responses.
     public var responseCode: DNSResponseCode {
-        DNSResponseCode(rawValue: parameters & Mask.rcode)
+        DNSResponseCode(rawValue: parameters.rawValue & Parameters.rcode.rawValue)
     }
 
     /// QDCOUNT an unsigned 16 bit integer specifying the number of entries in the question section.
@@ -117,7 +123,7 @@ public struct DNSHeader: Hashable, Sendable, Identifiable {
     /// - Parameter buffer: DNS response buffer.
     init(buffer: inout UnsafeBuffer) throws {
         self.id = try buffer.readInteger(UInt16.self)
-        self.parameters = try buffer.readInteger(UInt16.self)
+        self.parameters = Parameters(rawValue: try buffer.readInteger(UInt16.self))
         self.questionCount = try buffer.readInteger(UInt16.self)
         self.answerCount = try buffer.readInteger(UInt16.self)
         self.authorityCount = try buffer.readInteger(UInt16.self)
@@ -126,7 +132,7 @@ public struct DNSHeader: Hashable, Sendable, Identifiable {
 
     func write(to buffer: inout UnsafeBuffer) {
         buffer.write(id)
-        buffer.write(parameters)
+        buffer.write(parameters.rawValue)
         buffer.write(questionCount)
         buffer.write(answerCount)
         buffer.write(authorityCount)
@@ -155,3 +161,12 @@ extension DNSHeader: CustomStringConvertible {
         ].joined(separator: ", ")
     }
 }
+
+// MARK: - ExpressibleByIntegerLiteral
+
+extension DNSHeader.Parameters: ExpressibleByIntegerLiteral {
+    init(integerLiteral value: UInt16) {
+        self.rawValue = value
+    }
+}
+
